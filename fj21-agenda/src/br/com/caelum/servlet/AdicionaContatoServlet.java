ab1a7eefb747a5ee39f8a4050fb257ce3f06cf16
danielpfernandes <daniel.paivafernandes@gmail.com>
2018-06-02 08:37:24 -0300

    Exercícios do Capítulo 11 ajustado

--- fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
+++ fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
@@ -2,6 +2,7 @@ package br.com.caelum.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -35,7 +36,7 @@ public class AdicionaContatoServlet extends HttpServlet {
 			dataNascimento = Calendar.getInstance();
 			dataNascimento.setTime(date);
 		} catch (java.text.ParseException e) {
-			out.println("Erro de conversáoO de data");
+			out.println("Erro de conversão de data");
 			return;
 		}
 		
@@ -46,7 +47,8 @@ public class AdicionaContatoServlet extends HttpServlet {
 		contato.setEndereco(endereco);
 		contato.setDataNascimento(dataNascimento);
 		
-		ContatoDao dao = new ContatoDao();
+		Connection connection = (Connection) request.getAttribute("conexao");
+		ContatoDao dao = new ContatoDao(connection);
 		dao.adiciona(contato);
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/contato-adicionado.jsp");
